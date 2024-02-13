40acb211f11ad2d7f7b6f13511dc8e4c26930345
kruhsilpatel01 <krushilchabhadiya600@gmail.com>
2024-02-13 17:49:19 +0530

    create a site

---
+++ main.js
@@ -0,0 +1,31 @@
+// active header
+let nav = document.querySelector("navigation-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    }
+    else {
+        nav.classList.remove("scrollon");
+    }
+}
+// counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start;
+        range = end - start;
+        increment = end > start ? 1 : -1,
+            step = Math.abs(math.floor(deration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("counter", 0, 1287, 3000);
+    counter("counter", 100, 5786, 2500);
+    counter("counter", 0, 1440, 3000);
+    counter("counter", 0, 7110, 3000);
+});
